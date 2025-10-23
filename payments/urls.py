ffc4f75716b6e4bae622ea7f426eac062c0e7ae6
ritu207 <rituislam207@gmail.com>
2025-10-23 18:23:22 +0600

    implement views for payment

--- payments/urls.py
+++ payments/urls.py
@@ -8,10 +8,3 @@ urlpatterns = [
     path('success/<int:payment_id>/', views.payment_success, name='payment_success'),
 ]
 
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('pay/', views.make_payment, name='make_payment'),
-    path('success/', lambda r: render(r, 'payments/success.html'), name='payment_success'),
-]
