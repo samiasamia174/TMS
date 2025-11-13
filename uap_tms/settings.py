6589eb15effd13ddf8a03d2d35cc3e6e6c0690bd
Samia Zaman <samiazaman174@gmail.com>
2025-11-13 23:56:50 +0600

    modified:   uap_tms/settings.py

--- uap_tms/settings.py
+++ uap_tms/settings.py
@@ -74,15 +74,8 @@ WSGI_APPLICATION = 'uap_tms.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'uap_tms_db',
-        'USER': 'root',
-        'PASSWORD': 'root123',  # Change this to your MySQL password
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
