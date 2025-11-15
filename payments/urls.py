2e12eb56c508d4bde71a3889aab1beeddad98bdd
ritu207 <rituislam207@gmail.com>
2025-11-15 13:56:43 +0600

    update payment urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,5 @@
 # payments/urls.py
-from django.urls import path
-from . import views
-
 urlpatterns = [
-    path('payment/', views.make_payment, name='make_payment'),
+    path('payment/<int:bus_id>/', views.make_payment, name='make_payment'),
     path('payment/success/', views.payment_success, name='payment_success'),
 ]
