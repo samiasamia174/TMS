6cee3decd42d55206eeb1b025812c6680ff0fc3d
ritu207 <rituislam207@gmail.com>
2025-11-15 13:05:34 +0600

    error fix in views

--- payments/views.py
+++ payments/views.py
@@ -8,3 +8,11 @@ from django.contrib.auth.models import User
 from buses.models import Bus
 from .models import Payment
 import uuid
+
+def make_payment(request):
+    if request.method == 'POST':
+        student_name = request.POST.get('student_name')
+        student_id = request.POST.get('student_id')
+        vehicle_type = request.POST.get('vehicle_type')
+        amount = request.POST.get('amount', '500')
+        email = request.POST.get('email')
